1c4aa79e8cd1c27989712000436ff15959f9ab76
lnguyen2693 <n9linh@gmail.com>
2024-02-13 00:34:26 -0500

    refactor Engine class

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,13 +1,24 @@
-from abc import ABC
-
-from car import Car
+from engine.engine import Engine
 
 
-class CapuletEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
-        self.current_mileage = current_mileage
+class CapuletEngine(Engine):
+    def __init__(self, current_milage, last_service_mileage):
         self.last_service_mileage = last_service_mileage
+        self.current_milage = current_milage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 30000
+    def need_service(self):
+        return self.current_milage - self.last_service_mileage > 30000
+
+# from abc import ABC
+
+# from car import Car
+
+
+# class CapuletEngine(Car, ABC):
+#     def __init__(self, last_service_date, current_mileage, last_service_mileage):
+#         super().__init__(last_service_date)
+#         self.current_mileage = current_mileage
+#         self.last_service_mileage = last_service_mileage
+
+#     def engine_should_be_serviced(self):
+#         return self.current_mileage - self.last_service_mileage > 30000
